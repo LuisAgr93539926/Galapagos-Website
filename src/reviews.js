d6de06e88c167641e2df129df29c7669e5b317a0
lmagreda-sanchez <lmagreda-sanchez@myseneca.ca>
2023-08-13 20:55:37 -0400

    Finishing the more tab

--- src/reviews.js
+++ src/reviews.js
@@ -1,86 +1,85 @@
 window.reviews = [
-    {
-      id: 1,
-      name: "Alex Broere",
-      date: 1672444800000, // 5/1/2023 
-      rating: 5,
-      description:"Galapagos islands has a special place in my heart. It's such a unique place on earth. Seeing so many animals in such a small area is unique. Seals, penguins, sharks, crabs, birds, turtles etc. Amazing place and don't forget the story of Charles Darwin. Treat the place with respect! Don't ever touch animals and don't get to close to them.",
-      link: [
-        {
-            url: "https://goo.gl/maps/emeb7eXdEL2faqQv9",
-            urlName: "Click to see review"
-        }
-    ]
-    
-
-
-
-
-
-    },
-    {
-        id: 2,
-        name: "ΓΡΗΓΟΡΗΣ ΜΑΡΓΑΡΙΤΗΣ",
-        date: 1674211200000, // 7/1/2023 
-        rating: 5,
-        description:"In my opinion i think because of his special biodiversity it is the most unique island of all over the world whatever you say about this island would be very little in front of the beauty you face reaching to this and explore it",
-        link: [
-          {
-              url: "https://goo.gl/maps/ejYX1CDZFKSY3pQQ6",
-              urlName: "Click to see review"
-          }
-      ]
-    },
-    {
-        id: 3,
-        name: "Mark Mulder",
-        date: 1671504000000, // 4/1/2023 
-        rating: 5,
-        description:"Secluded and natural- Darwin knew where to find paradise 😎👌",
-        link: [
-          {
-              url: "https://goo.gl/maps/dePzqDtRKRr2cYy96",
-              urlName: "Click to see review"
-          }
-      ]
-    },
-    {
-        id: 4,
-        name: "Sushobhan Chowdhury",
-        date: 1674384000000, // 8/1/2023 
-        rating: 5,
-        description:"Lovely photography",
-        link: [
-            {
-                url: "https://goo.gl/maps/YGDbXkqBDvMJ2PU18",
-                urlName: "Click to see review"
-            }
-        ]
-    },
-    {
-        id: 5,
-        name: "Christopher Woofter",
-        date: 1619827200000, // 5/1/2021 
-        rating: 5,
-        description:"Home to many different types of tortoises.  A rather inhospitable and landscape for humans.   Many diverse life forms live here.",
-        link: [
-          {
-              url: "https://goo.gl/maps/AMULH6mU6kN2qkC2A",
-              urlName: "Click to see review"
-          }
-      ]
-    },
-    {
-        id: 6,
-        name: "Andrew Smith",
-        date: 1619827200000, // 5/1/2021 
-        rating: 5,
-        description:"Absolutely amazing,  got lucky and found a family that was familiar with the area.  Made a good vacation choice great to be able to make friends with the people who inhabit the Island(s).                                I will return God willing  !",
-        link: [
-          {
-              url: "https://goo.gl/maps/AMULH6mU6kN2qkC2A",
-              urlName: "Click to see review"
-          }
-      ]
-    },
+  {
+    id: 1,
+    name: "Alex Broere",
+    date: 1672444800000, // 5/1/2023
+    rating: 5,
+    description:
+      "Galapagos islands has a special place in my heart. It's such a unique place on earth. Seeing so many animals in such a small area is unique. Seals, penguins, sharks, crabs, birds, turtles etc. Amazing place and don't forget the story of Charles Darwin. Treat the place with respect! Don't ever touch animals and don't get to close to them.",
+    link: [
+      {
+        url: "https://goo.gl/maps/emeb7eXdEL2faqQv9",
+        urlName: "Click to see review",
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: "ΓΡΗΓΟΡΗΣ ΜΑΡΓΑΡΙΤΗΣ",
+    date: 1674211200000, // 7/1/2023
+    rating: 5,
+    description:
+      "In my opinion i think because of his special biodiversity it is the most unique island of all over the world whatever you say about this island would be very little in front of the beauty you face reaching to this and explore it",
+    link: [
+      {
+        url: "https://goo.gl/maps/ejYX1CDZFKSY3pQQ6",
+        urlName: "Click to see review",
+      },
+    ],
+  },
+  {
+    id: 3,
+    name: "Mark Mulder",
+    date: 1671504000000, // 4/1/2023
+    rating: 5,
+    description:
+      "Secluded and natural- Darwin knew where to find paradise 😎👌",
+    link: [
+      {
+        url: "https://goo.gl/maps/dePzqDtRKRr2cYy96",
+        urlName: "Click to see review",
+      },
+    ],
+  },
+  {
+    id: 4,
+    name: "Sushobhan Chowdhury",
+    date: 1674384000000, // 8/1/2023
+    rating: 5,
+    description: "Lovely photography",
+    link: [
+      {
+        url: "https://goo.gl/maps/YGDbXkqBDvMJ2PU18",
+        urlName: "Click to see review",
+      },
+    ],
+  },
+  {
+    id: 5,
+    name: "Christopher Woofter",
+    date: 1619827200000, // 5/1/2021
+    rating: 5,
+    description:
+      "Home to many different types of tortoises.  A rather inhospitable and landscape for humans.   Many diverse life forms live here.",
+    link: [
+      {
+        url: "https://goo.gl/maps/AMULH6mU6kN2qkC2A",
+        urlName: "Click to see review",
+      },
+    ],
+  },
+  {
+    id: 6,
+    name: "Andrew Smith",
+    date: 1619827200000, // 5/1/2021
+    rating: 5,
+    description:
+      "Absolutely amazing,  got lucky and found a family that was familiar with the area.  Made a good vacation choice great to be able to make friends with the people who inhabit the Island(s).                                I will return God willing  !",
+    link: [
+      {
+        url: "https://goo.gl/maps/AMULH6mU6kN2qkC2A",
+        urlName: "Click to see review",
+      },
+    ],
+  },
 ];
